570da9130d6e58adf086c2f9c7319e7789120c98
phil <phil@polarstork.com>
2023-03-20 18:24:05 +0200

    init

---
+++ git_tracking.sh
@@ -0,0 +1,22 @@
+# Set the branch name and file name
+branch="master"
+file="tracking.txt"
+
+while true; do
+  # Pull the latest changes from the branch
+  git pull --quiet origin $branch:$branch > /dev/null && \
+
+  # Echo the current date and time to the file
+  echo $(date) > $file && \
+
+  # Commit and push changes
+  git add $file > /dev/null && \
+  git commit -m "tracking" > /dev/null && \
+  git push --quiet origin $branch > /dev/null
+
+  # Notify
+  printf "\rtracking $(date)\n"
+
+  # Wait for the default sleep time of 5 minutes
+  sleep 300
+done
---
+++ readme.md
@@ -0,0 +1,11 @@
+A simple work time tracker method.
+
+### How-To:
+
+1. Fork & clone this repository.
+2. using your terminal, authorise the sh file to run using chmod
+   > chmod 777 git_tracking.sh
+3. then run the script type
+   > git_tracking.sh
+
+this will commit and push every X seconds to your repository until you close the terminal.
